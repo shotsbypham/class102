898de0b8679193009e6fa008c8abbe36b4fb5925
Trina Pham <tqpham02@gmail.com>
2022-07-09 03:19:27 -0700

    added dynamic components

---
+++ app.js
@@ -0,0 +1,18 @@
+let firstName = prompt("What is your first name?");
+console.log(firstName);
+document.write(firstName + ", welcome to a little sneak peek on my life!")
+
+if (firstName == "Jason"){
+    document.write("hi mr. jason cameron lee");
+} else if (firstName == "marcus") {
+    document.write(" marcus bad, marcus so so bad!")
+}
+
+
+let age = prompt("How are old are you?")
+console.log(age);
+document.write(" You are " + age + " years old.")
+
+let icecream = prompt("What is your favorite icecream flavor?")
+console.log(icecream);
+document.write(" that icecream flavor is foul.")
